ff0943c918bfa5bc68ca0e3c583a2f50c2603c58
gokalpfirat <gokalpfirat@gmail.com>
2023-09-30 13:29:51 +0300

    solution

---
+++ src/components/ToastProvider/ToastProvider.js
@@ -0,0 +1,42 @@
+import React from "react";
+
+import useKeydown from "../../hooks/use-keydown";
+
+export const ToastContext = React.createContext();
+
+function ToastProvider({ children }) {
+  const [toasts, setToasts] = React.useState([]);
+
+  function createToast(message, variant) {
+    const nextToasts = [
+      ...toasts,
+      { id: crypto.randomUUID(), message, variant },
+    ];
+    setToasts(nextToasts);
+  }
+
+  function removeToast(id) {
+    const nextToasts = toasts.filter((toast) => toast.id !== id);
+    setToasts(nextToasts);
+  }
+
+  const handleEscape = React.useCallback(() => {
+    setToasts([]);
+  }, []);
+
+  useKeydown("Escape", handleEscape);
+
+  return (
+    <ToastContext.Provider
+      value={{
+        toasts,
+        createToast,
+        removeToast,
+      }}
+    >
+      {children}
+    </ToastContext.Provider>
+  );
+}
+
+export default ToastProvider;
